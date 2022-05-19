91c7077d07a3ba4d94030a7a38fa8c2dbf586f9d
Mark Kaylor <mark.kaylor@strapi.io>
2022-05-19 15:25:25 +0200

    setup

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'a28c57f870e22fb84d71069efd083844'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '9b433013-79bb-4b01-b5ef-43c011782b65'
+};
